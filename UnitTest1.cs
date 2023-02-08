d8bcc66c41682643c2a474fc5997a1be23ba6eac
eric <eric.guilbaud@gmail.com>
2023-02-08 21:04:27 +0100

    utilisation de impoter +  passage en non core :'(

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,6 +1,7 @@
 using Stargate.Stargate;
 using System.Runtime.CompilerServices;
 using Stargate.SGGodot;
+using Stargate;
 
 namespace TestProject1
 {
@@ -10,11 +11,14 @@ namespace TestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            
             CardImporter ci = new CardImporter() { Path = "../../../" + Const.SetFile };
-            ci.Process();
+            ci.Load();
             Assert.AreEqual(292, ci.list.ToList().Count);
-            
+
+            SGCard c = ci.GetCardFromGUID("79974bc9-9b81-41e1-8868-c75f8fc58837");
+            Assert.AreEqual(c.Id, "79974bc9-9b81-41e1-8868-c75f8fc58837");
+
+            Assert.ThrowsException<Exception>(()  =>  ci.GetCardFromGUID("pouet"));
 
         }
     }
