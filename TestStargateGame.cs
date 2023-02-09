bf26a688544bd83a2fc3ff552d6cee833ae78ddb
eric <eric.guilbaud@gmail.com>
2023-02-09 13:14:00 +0100

    maj

--- TestStargateGame.cs
+++ TestStargateGame.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 using Stargate.SGGodot;
 using Stargate;
 using Stargate.Mock;
+using Stargate.Stargate.Enum;
 
 namespace TestProject1
 {
@@ -13,14 +14,14 @@ namespace TestProject1
         public void TestMethod1()
         {
             Library library = new Library(TestConstants.SETPATH);
-            StargateGame stargateGame = new StargateGame() { player1 = new Player(), Library = library };
+            StargateGame stargateGame = new StargateGame(1, library) ;
             StargateGameMock.InitPlayer1WithMock(stargateGame);
 
+            
+            
+            StargateResult StargateResult = stargateGame.CardService.PlayMission(stargateGame.player1);
+            Assert.AreEqual(StargateResult.actionResult, ActionResult.Success);
 
-
-
-
-            Assert.IsTrue(true);
 
         }
     }
