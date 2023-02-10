b2ccfcbf6d254dcf6e8c7a18c2afcd1c413da346
eric <eric.guilbaud@gmail.com>
2023-02-10 16:40:01 +0100

    event qui marchouilles

--- TestHandler.cs
+++ TestHandler.cs
@@ -3,24 +3,31 @@ using System.Runtime.CompilerServices;
 using Stargate.SGGodot;
 using Stargate;
 using Stargate.StateMachine;
+using System.Diagnostics;
 
 namespace TestProject1
 {
     [TestClass]
-    public class CardImporter
+    public class TestHandler
     {
         [TestMethod]
         public void TestMethod1()
         {
 
-
-
             //InitPhase p = new InitPhase(new GameState() { CurrentPlayer = new Player(), CardService = new Stargate.Service.CardService()});
-            Phase p = new InitPhase(new GameState() { CurrentPlayer = new Player(), CardService = new Stargate.Service.CardService() });
+            InitPhase p = new InitPhase(new GameState() { CurrentPlayer = new Player(), CardService = new Stargate.Service.CardService() });
+            p.StargateResultHandler += HandlerEventTestMethod;
 
             p.Run();
 
 
         }
+
+        public void HandlerEventTestMethod(object sender, EventArgs e)
+        {
+            Debug.WriteLine(e);
+        }
+
+
     }
 }
