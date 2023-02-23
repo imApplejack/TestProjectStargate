5606a04b63a8e54b331e926167fc6d94857c1142
eric <eric.guilbaud@gmail.com>
2023-02-23 12:00:02 +0100

    v0 de la machine à etat

--- TestHandler.cs
+++ TestHandler.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using Stargate.Stargate.Enum;
 using Stargate.Stargate.Event;
 
+
 namespace TestProject1
 {
     [TestClass]
@@ -16,35 +17,33 @@ namespace TestProject1
         public void TestMethod1()
         {
 
-            //InitPhase p = new InitPhase(new GameState() { CurrentPlayer = new Player(), CardService = new Stargate.Service.CardService()});
-            Phase p = new InitPhase(new GameState() { player1 = new Player(), player2 = new Player(), CardService = new Stargate.Service.CardService() });
-            p.gameState.InitGame(1);
-
-            p.gameState.StargateResultHandler += HandlerEventTestMethod;
-
-            p.Run();
+            //StateAction myAction = new StateAction();
+           // myAction.Play();
 
 
 
-
-            QuestPhase mp = new QuestPhase((p.gameState));
-            Assert.IsInstanceOfType(p.gameState.GameStack.Peek(), mp.GetType());
-            Assert.AreEqual(3, p.gameState.CurrentPlayer.Energy);
+            //StatePhase myPhase = new StatePhase();
+            //myPhase.AddAction(new StateAction());
 
 
 
+            //StatePhase mySubPhase = new StatePhase();
+            //mySubPhase.AddAction(new StateAction()).AddAction(new StateAction());
+            
+            
+            //mySubPhase.AddAction(new StateAction());
+            //myPhase.AddAction(mySubPhase);
+            
+            
+            
+           InitPhase  i = new InitPhase();
+            i.Play();   
 
-
-            //p.gameState.ProcessEvent(new PlayCardEvent() { })
 
 
         }
 
-        public void HandlerEventTestMethod(object sender, EventArgs e)
-        {
-         //   ((StargateResult)e).actionResult = ActionResult.Failure;
-            Debug.WriteLine(e);
-        }
+        
 
 
     }
